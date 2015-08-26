60df64828bcbb865ef4747822e75db6b660d75b3
nick <w19900227@gmail.com>
2015-08-26 17:02:54 +0800

    [Modify]main6 on 00_useful dir

--- src/00_useful/main6.go
+++ src/00_useful/main6.go
@@ -8,11 +8,11 @@ import (
 
 type ex1 ex1_1
 type ex1_1 struct {
-	Test []string
+	E1 []string
 }
 
 func ex1_func(w http.ResponseWriter, r *http.Request) {
-	e1 := ex1{[]string{"aaa","bbb"}}
+	e1 := ex1{[]string{"ex1-1","ex1-2"}}
 
 	js, err := json.Marshal(e1)
 	if err != nil {
@@ -26,11 +26,11 @@ func ex1_func(w http.ResponseWriter, r *http.Request) {
 
 type ex2 []ex2_1
 type ex2_1 struct {
-	Name string
+	E2 string
 }
 
 func ex2_func(w http.ResponseWriter, r *http.Request) {
-	e2 := ex2{{"aa"}, {"bb"}}
+	e2 := ex2{{"ex2-1"}, {"ex2-2"}}
 
 	js, err := json.Marshal(e2)
 	if err != nil {
@@ -44,34 +44,58 @@ func ex2_func(w http.ResponseWriter, r *http.Request) {
 
 
 func ex3_func(w http.ResponseWriter, r *http.Request) {
-    var ta []interface{}
-    type tt struct {
+    var Datas []interface{}
+    type Data struct {
         Name string `json:"name"`
         Age int `json:"age"`
     }
-    for i:=1; i<3; i++ {
-        ta = append(ta, tt{"aaa", i})
-        // tt.Name = "aaa"
-        // tt.Age = i
-        // ta = append(ta, tt)
-    }
-    // ta := tt{"aaa", 2}
-    // fmt.Println(ta)
 
-    js, err := json.Marshal(ta)
+    for i:=1; i<3; i++ {
+        Datas = append(Datas, Data{"ex3", i})
+    }
+
+    ret, err := json.Marshal(Datas)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
     w.Header().Set("Content-Type", "application/json")
-    w.Write(js)
+    w.Write(ret)
+}
+
+func ex4_func(w http.ResponseWriter, r *http.Request) {
+    type Data struct {
+        Name string `json:"name"`
+        Age int `json:"age"`
+    }
+    type Datas []Data 
+        
+    result := Datas{}
+    var data Data
+    for i:=1; i<3; i++ {
+        data.Name = "ex4"
+        data.Age = i
+        result = append(result, data)
+        
+    }
+
+    ret, err := json.Marshal(result)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(ret)
 }
 
 func main() {
-	http.HandleFunc("/", ex1_func)
+	http.HandleFunc("/", ex4_func)
+	http.HandleFunc("/ex1", ex1_func)
 	http.HandleFunc("/ex2", ex2_func)
 	http.HandleFunc("/ex3", ex3_func)
+	http.HandleFunc("/ex4", ex4_func)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe : ", err)
